5df9efb40bb5ba52cdf5259ba8ceeded6582d7e5
gamemerald <77945842+gamemerald@users.noreply.github.com>
2021-01-25 18:07:57 -0600

    Create patch.bat

---
+++ vbox/patch.bat
@@ -0,0 +1,8 @@
+cd "C:\Program Files\Oracle\VirtualBox\"
+VBoxManage.exe modifyvm "vm_name" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+VBoxManage setextradata "vm_name" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
+VBoxManage setextradata "vm_name" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+VBoxManage setextradata "vm_name" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
+VBoxManage setextradata "vm_name" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+VBoxManage setextradata "vm_name" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+pause
